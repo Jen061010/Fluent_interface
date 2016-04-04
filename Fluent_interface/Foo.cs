cdd435d8218e2d4d6d5613e4117aacd8e1d64571
Jennifer <jvs1192@gmail.com>
2016-04-04 13:03:42 +0200

    First Commit

---
+++ Fluent_interface/Foo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluent_interface
+{
+    public class Foo
+    {
+        //private string _fields;
+        //private string _where;
+        public Foo Select() //string fields){
+        {
+            //_fields=fields
+            return this; 
+        }
+        public Foo Where() 
+        {
+            //_where=where;
+            return this;
+        }/* 
+          * public string Execute()
+          * {
+          *     return "Select "+""+_fields+""+_where;
+          * }*/
+
+    }
+}
---
+++ Fluent_interface/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fluent_interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            foo.Select().Where();
+            //foo.Select(_fields).Where(_where).Execute();
+            Console.WriteLine(foo);
+        }
+    }
+}
